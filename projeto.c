c0bcbc402379afba5fa2e731a3b1611b7921af1b
chambelpower <74711832+chambelpower@users.noreply.github.com>
2022-04-16 20:16:41 +0100

    Update projeto.c

--- projeto.c
+++ projeto.c
@@ -18,6 +18,7 @@ int readFile(char* file);
 void print_SHM();
 void systemManager();
 
+
 pid_t systemManagerPID;
 
 typedef struct _s1{
@@ -183,20 +184,20 @@ void taskManager(){
 	
 	sem_init(&mutexFila, 0, 1);
 	sem_wait(&mutexFila);
-	task* t = (task*)malloc(sizeof(task));
+	tasks* t = (tasks*)malloc(sizeof(tasks));
 	t->id = -1;// e utilizado -1 quando o espaco na lista nao esta ocupado
 	t->next = NULL;
 	filaT->taskList = t;
 	for(int i = 1; i < sh_mem->slots; i++){
-		task *t1 = (task*)malloc(sizeof(task));
-		t1->id = -i;
-		t1->NULL;
+		tasks *t1 = (tasks*)malloc(sizeof(tasks));
+		t1->id = -1;
+		t1->next = NULL;
 		t->next = t1;
 		t = t1;
 	}
 	sem_post(&mutexFila);
 
-	print_SHM2();
+	
 	
 	while(1){
 		fd = open(task_pipe, O_RDONLY);
@@ -224,14 +225,7 @@ void maintenanceManager(){
 	exit(0);
 }
 
-void print_SHM2(){
-	printf("SHM2:\n");
-	tasks* t = filaT->taskList;
-	while(t != NULL){
-		printf("ID: %d\n", t->id);
-		t=t->next;
-	}
-}
+
 
 void print_SHM(){
 	
