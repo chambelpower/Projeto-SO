b129dbbc6f85e5ebe4e0b4edfc0e14fc25144001
chambelpower <74711832+chambelpower@users.noreply.github.com>
2022-04-28 18:12:18 +0100

    Update projeto.c

--- projeto.c
+++ projeto.c
@@ -27,7 +27,9 @@ void systemManager();
 void print_SHM2();
 void insertFila(int id, int n_inst, int tMax);
 void cleanup();
-
+void print_lista_tarefas();
+void printServers();
+void printServers2();
 pid_t systemManagerPID;
 
 enum serverState{
@@ -96,10 +98,13 @@ struct tm* getTime() {
 	return tm_struct;
 }
 
-
+struct timeval timeout;
 
 pthread_mutex_t mutexFila = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t scheduler_cs= PTHREAD_COND_INITIALIZER;
+pthread_cond_t dispatcher_cs= PTHREAD_COND_INITIALIZER;
+
+pthread_mutex_t mutexUnnamedPipe = PTHREAD_MUTEX_INITIALIZER;
 
 void logFile(char msg[100]) {
 	sem_wait(logSem);
@@ -153,6 +158,7 @@ int readFile(char* file){
 			e->capacidade1 = atoi(value);
 			value = strtok(NULL, ",");
 			e->capacidade2 = atoi(value);
+			e->state = 5;
 			e->next = NULL;
 		}
 	else{
@@ -171,6 +177,7 @@ int readFile(char* file){
 			e1->capacidade1 = atoi(value);
 			value = strtok(NULL, ",");
 			e1->capacidade2 = atoi(value);
+			e1->state = 5;
 			e1->next = NULL;
 			e->next = e1;
 			e = e1;
@@ -180,57 +187,169 @@ int readFile(char* file){
 		}
 		i++;
 	} 
-	
+	sh_mem->highPerformanceFlag = 0;
 	sem_post(mutex);
 	fclose(f);
 	
 	return 0;
 }
 
-void *cpu(void *t){
-	//int my_id = *((int *)t);
-
-	printf("CPU is READY\n");
+void *cpu1(void *t){
+	printf("CPU1 is READY\n");
+	int counter = *((int *)t);
+	int i = 0;
+	sem_wait(mutex);
+	edge_server* e = sh_mem->edgeList;
+	while(i < counter){
+		e = e->next;
+		i++;
+	}	
+	
+	close(e->channel[1]);
+	sem_post(mutex);
+	fd_set read_set;
+	
+	tasks* t1;
 	while(1){
+		
+		
 		sem_wait(mutex);
-		if()
+	
+		if(e->state == RUNNING){
+		
+			
+			pthread_mutex_lock(&mutexUnnamedPipe);
+		
+			FD_ZERO(&read_set);
+			
+			FD_SET(e->channel[0], &read_set);
+			
+			if(select(e->channel[0]+1, &read_set, NULL, NULL, &timeout) > 0){
+				read(e->channel[0], &t1, sizeof(tasks));
+				printf("CPU1 RECEBEU TASK\n");
+				sem_post(mutex);
+			}
+			else{
+				sem_post(mutex);
+			}
+			pthread_mutex_unlock(&mutexUnnamedPipe);
+			
+		}
+		else{
+			sem_post(mutex);
+		}
 	}
 	
 	pthread_exit(NULL);
 }
 
-void edgeServer(char *name, int capMin, int capMax){
-	printf("%s STARTED\n", name);
-	logFile(strcat(name, " READY"));
+void *cpu2(void *t){
 	
-	int i;
-	int id[NCPU];
-	pthread_t threads[NCPU];
+	printf("CPU2 is READY\n");
+	int counter = *((int *)t);
+	int i = 0;
+	sem_wait(mutex);
+	edge_server* e = sh_mem->edgeList;
+	while(i < counter){
+		e = e->next;
+		i++;
+	}	
 	
-	for(i = 0; i < NCPU; i++){
-		id[i] = i;
-		pthread_create(&threads[i], NULL, cpu, &id[i]);
+	close(e->channel[1]);
+	sem_post(mutex);
+	fd_set read_set;
+	tasks* t1;
+	while(1){
+		sem_wait(mutex);
+		if(e->state == RUNNING && sh_mem->highPerformanceFlag == 1){
+			
+			pthread_mutex_lock(&mutexUnnamedPipe);
+			FD_ZERO(&read_set);
+			FD_SET(e->channel[0], &read_set);
+			if(select(e->channel[0]+1, &read_set, NULL, NULL, &timeout) > 0){
+				read(e->channel[0], &t1, sizeof(tasks));
+				printf("CPU2 RECEBEU TASK\n");
+				sem_post(mutex);
+			}
+			else{
+				sem_post(mutex);
+			}
+			pthread_mutex_unlock(&mutexUnnamedPipe);
+		}
+		else{
+			sem_post(mutex);
+		}
 	}
+	
+	pthread_exit(NULL);
+}
+void changeServerName(int counter, char *str){
+	int i = 0;
+	edge_server* serverList = sh_mem->edgeList;
+	while(i < counter){
+		serverList=serverList->next;
+		i++;
+	}
+	strcat(serverList->name, str);
+	sh_mem->highPerformanceFlag++;
+	
+	
+}
+void edgeServer(edge_server *e, int counter){
+	int i;
+	char var[100];
+	pthread_t threads[NCPU];
+	//printServers();
 	sem_post(serverMutex);
+	sem_wait(mutex);
+	printf("%s STARTED\n", e->name);
+	strcpy(var, e->name);
+	logFile(strcat(var, " READY"));//nao pode ser assim, esta a adicionar READY a variavel na memoria partilhada
+	
+
+	printf("NAME: %s\n", e->name);
+	
+	e->state = RUNNING;
+	printf("STATE: %d\n", e->state);
+	
+	changeServerName(counter, "DEBUG");
+	sem_post(mutex);
+	//printServers();
+	pthread_create(&threads[0], NULL, cpu1, &counter);
+	pthread_create(&threads[1], NULL, cpu2, &counter);
+	
+	
+	printf("DEBUG5\n");
+	
+  	printServers();
 	for(i = 0; i < NCPU; i++){
 		pthread_join(threads[i], NULL);
   	}
+  	
 }
 
 void removeTarefasImpo__Prio(){
+	//print_lista_tarefas();
 	tasks* t = taskList;
 	int timeAtual = (int)time(NULL);
 	while(t != NULL){
-		if(t->data->id != -1 && t->data->tMax >= timeAtual){
-			t->data->id = -1;//-1 representa que o lugar na lista nao esta ocupado
-			printf("Task Removed\n");
-			logFile("TASK REMOVED");
+		if(t->data->id == -1){
+			t->data->prio = 1000;
 		}
 		else{
-			t->data->prio = t->data->tMax - timeAtual;//no maximo este valor pode ser 1, desempatado pela ordem de chegada
+			if(t->data->tMax <= timeAtual){
+				t->data->id = -1;//-1 representa que o lugar na lista nao esta ocupado
+				t->data->prio = 1000;
+				printf("Task Removed: tMax->%d, timeAtual->%d\n", t->data->tMax, timeAtual);
+				logFile("TASK REMOVED");
+			}
+			else{
+				t->data->prio = t->data->tMax - timeAtual;//no maximo este valor pode ser 1, desempatado pela ordem de chegada
+			}
 		}
 		t=t->next;
 	}
+	//print_lista_tarefas();
 }
 
 
@@ -262,15 +381,33 @@ void ordenar(){
 	}
 }
 
+void print_lista_tarefas(){
+
+	tasks* current = taskList;
+	printf("LISTA DE TAREFAS\n");
+	while(current != NULL){
+		printf("ID: %d, PRIO: %d\n", current->data->id, current->data->prio);
+		current = current->next;
+	}
+	
+}
+
 void *scheduler(void *t){
 	printf("Starting scheduler\n");
+	pthread_mutex_lock(&mutexFila);
+	//print_lista_tarefas();
+	pthread_mutex_unlock(&mutexFila);
 	while(1){
 		pthread_mutex_lock(&mutexFila);
 		pthread_cond_wait(&scheduler_cs, &mutexFila);
 		printf("RECEBEU SINAL\n");
-		
+		//print_lista_tarefas();
 		removeTarefasImpo__Prio();
+		//printf("DEBUG1\n");
+		//print_lista_tarefas();
 		ordenar();
+		//printf("DEBUG2\n");
+		//print_lista_tarefas();
 		pthread_mutex_unlock(&mutexFila);
 		
 	}
@@ -280,15 +417,18 @@ void *scheduler(void *t){
 int verificaExistirTarefas(){
 	tasks* t = taskList;
 	while(t != NULL){
-		if(id != -1){
+		if(t->data->id != -1){
+			printf("ENCONTROU TAREFA\n");
 			return 1;
 		}	
 		t=t->next;
 	}
+	printf("NAO ENCONTROU TAREFA\n");
 	return 0;
 }
 
 int tryDispatchTask(tasks* t){
+	printf("TRY DISPATCH\n");
 	sem_wait(mutex); 
 	edge_server* serverList = sh_mem->edgeList;
 	while(serverList != NULL){
@@ -339,14 +479,66 @@ int taskValida(tasks* t){
 	return -1;
 }
 
+int verificaCPU(){
+	sem_wait(mutex);
+	edge_server* serverList = sh_mem->edgeList;
+	while(serverList != NULL){
+		//printf("DEBUG0: %s\n", serverList->name);
+		//printf("DEBUG1: %d\n", serverList->state);
+		//printf("DEBUG2: %d\n", STOPPED);
+		//printServers2();
+		if(serverList->state == RUNNING){
+			printf("ESTADO1: %d, ESTADO2: %d\n", serverList->cpuState1, serverList->cpuState2);
+			if(serverList->cpuState1 == AVAILABLE){
+				printf("ENCONTROU CPU1\n");
+				return 1;
+			}
+			if(sh_mem->highPerformanceFlag == 1){
+				if(serverList->cpuState2 == AVAILABLE){
+					printf("ENCONTROU CPU2\n");
+					return 2;
+				}
+			}	
+		}
+		serverList=serverList->next;
+	}
+	printf("FLAG: %d\n", sh_mem->highPerformanceFlag);
+	sem_post(mutex);
+	printf("NAO ENCONTROU CPU DISPONIVEL\n");
+	
+	return 0;
+}
+
+void printServers(){
+	sem_wait(mutex);
+	edge_server* serverList = sh_mem->edgeList;
+	while(serverList != NULL){
+		printf("%s, STATE: %d\n", serverList->name, serverList->state);
+		serverList=serverList->next;
+	}
+	sem_post(mutex);
+}
+
+void printServers2(){
+	
+	edge_server* serverList = sh_mem->edgeList;
+	while(serverList != NULL){
+		printf("%s, STATE: %d\n", serverList->name, serverList->state);
+		serverList=serverList->next;
+	}
+	
+}
+
 void *dispatcher(void *t){
 	printf("Starting dispatcher\n");
 	while(1){
-		pthread_mutex_lock(&mutexFila);
-		while(verificaExistirTarefas() == 0){
-			pthread_cond_wait(&dispatcher_cs, &mutexFila);
+		
+		while(verificaExistirTarefas() == 0 && verificaCPU() == 0){
+			sleep(2);
 		}
+		pthread_mutex_lock(&mutexFila);
 		tasks* t = taskList;
+		printf("DEBUG6\n");
 		while(t != NULL){//esta sempre ordenado por prioridade por isso apenas temos que percorrer e encontrar uma que seja possivel fazer
 			if(t->data->id != -1 && taskValida(t) == 1){
 				if(tryDispatchTask(t) == 1){
@@ -366,64 +558,66 @@ void *dispatcher(void *t){
 
 void taskManager(){
 	logFile("PROCESS TASK_MANAGER CREATED");
+	printf("PROCESS TASK_MANAGER CREATED\n");
 	int fd;
 	char str1[100];
+	
+	timeout.tv_sec = 0;
+	timeout.tv_usec = 10;
 	sem_wait(mutex); 
 	int totalServers = sh_mem->nEdgeServers;
 	edge_server* serverList = sh_mem->edgeList;
-	
 	sem_unlink("SERVERMUTEX");
 	serverMutex = sem_open("SERVERMUTEX", O_CREAT | O_EXCL, 0700, 1);
 	
 	for(int i = 0; i < totalServers; i++){
-		
-		
+		printf("here1\n");
 		sem_wait(serverMutex);
+		printf("here2\n");
 		pipe(serverList->channel);
 		if(fork() == 0){
-			sem_wait(mutex); 
-			char *name = serverList->name;
-			int a = serverList->capacidade1;
-			int b = serverList->capacidade2;
-			printf("Edge Server %s\n", name);
-			sem_post(mutex); 
-			edgeServer(name, a, b);	
 			
-			
+			printf("Edge Server %s\n", serverList->name);
+			edgeServer(serverList, i);	
 			exit(0);
 		}
-		
-		sem_wait(mutex); 
 		serverList = serverList->next;
-		sem_post(mutex); 
+		
 	}
+	
 	sem_post(mutex); 
+	
 	sem_wait(serverMutex);//para apenas avancar quando os servers todos tiverem preparados
 	printf("Servers ready\n");
 	sem_post(serverMutex);
 	//criacao da fila de tasks
-	
+	sleep(3);
+	printServers();
 	
 	pthread_mutex_lock(&mutexFila);
 	tasks* t = (tasks*)malloc(sizeof(tasks));
 	t->data = (data*)malloc(sizeof(data));
 	t->data->id = -1;// e utilizado -1 quando o espaco na lista nao esta ocupado
+	t->data->prio = 1000;
 	t->next = NULL;
 	taskList = t;
 	for(int i = 1; i < sh_mem->slots; i++){
 		tasks *t1 = (tasks*)malloc(sizeof(tasks));
 		t1->data = (data*)malloc(sizeof(data));
 		t1->data->id = -1;
+		t1->data->prio = 1000;
 		t1->next = NULL;
 		t->next = t1;
 		t = t1;
 	}
+	//print_lista_tarefas();
 	pthread_mutex_unlock(&mutexFila);
 
-	//int id[1];//para ja apenas a dispatcher
-  	pthread_t thread_id;
+	
+  	pthread_t thread_id[2];
   	
-	pthread_create(&thread_id, NULL, scheduler, NULL);
+	pthread_create(&thread_id[0], NULL, scheduler, NULL);
+	pthread_create(&thread_id[1], NULL, dispatcher, NULL);
 	
 	while(1){
 		fd = open(task_pipe, O_RDONLY);
@@ -451,7 +645,8 @@ void taskManager(){
 		}
 		close(fd);
 	}
-	pthread_join(thread_id, NULL);
+	pthread_join(thread_id[0], NULL);
+	pthread_join(thread_id[1], NULL);
 	exit(0);
 }
 
