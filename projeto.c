4902390976b702e040b396c7e52f8695fb210bb0
chambelpower <74711832+chambelpower@users.noreply.github.com>
2022-05-16 00:25:57 +0100

    Update projeto.c

--- projeto.c
+++ projeto.c
@@ -58,6 +58,8 @@ typedef struct _s1{
 	int timeNextTarefa2;
 	int state;
 	int channel[2];
+	int nTarefasRealizadas;
+	int nOperacoesManutencao;
 } edge_server;
 
 typedef struct _s2{
@@ -76,6 +78,10 @@ typedef struct{
 	int nEdgeServers;//numero de Edge Servers
 	int highPerformanceFlag;//fica a 1 quando for para os servers ficarem no modo highperformance
 	edge_server edgeList [MAX_SERVERS];
+	int totalTarefasRealizadas;
+	int tempoMedioDeEspera;
+	int tarefasMedioDeEspera;
+	int totalTarefasNaoRealizadas;
 } shm_struct;
 
 int shmid;
@@ -103,6 +109,7 @@ tasks *taskList;
 typedef struct{
 	int id;
 	int n_instru;
+	int tChegada;
 } taskSimplificada;
 
 struct tm* getTime() {
@@ -128,6 +135,14 @@ void logFile(char *msg) {
 	sem_post(logSem);
 }
 
+void logFile2(char *msg) {
+
+	FILE *r = fopen("debug.txt", "a");
+	fprintf(r, "%s\n", msg);
+	fclose(r);
+	
+}
+
 int readFile(char* file){
 	FILE *f = fopen(file, "r");
 	if(f == NULL){
@@ -135,8 +150,9 @@ int readFile(char* file){
 	}
 	char line[100];
 	int i = 0;
-	sem_wait(mutex);
 	
+	sem_wait(mutex);
+	//printf("WAIT1\n");
 	
 	while(i<3){
 		if(fgets(line, sizeof(line), f)){
@@ -145,9 +161,13 @@ int readFile(char* file){
 		switch(i){
 			case 0:
 				sh_mem->slots = atoi(newLine);
+				if(sh_mem->slots <= 0)
+					return -2;
 				break;
 			case 1:
 				sh_mem->maxWait = atoi(newLine);
+				if(sh_mem->maxWait <= 0)
+					return -2;
 				break;
 			case 2:
 				sh_mem->nEdgeServers = atoi(newLine);
@@ -167,11 +187,19 @@ int readFile(char* file){
 			char *newLine = strtok(line, "\n");
 			char *value = strtok(newLine, ",");
 			strcpy(sh_mem->edgeList[i].name, value);
+			if(strlen(sh_mem->edgeList[i].name) <= 0)
+				return -2;
 			value = strtok(NULL, ",");
 			sh_mem->edgeList[i].capacidade1 = atoi(value);
+			if(sh_mem->edgeList[i].capacidade1 <= 0)
+				return -2;
 			value = strtok(NULL, ",");
 			sh_mem->edgeList[i].capacidade2 = atoi(value);
+			if(sh_mem->edgeList[i].capacidade2 <= 0)
+				return -2;
 			sh_mem->edgeList[i].state = STOPPED;
+			sh_mem->edgeList[i].nTarefasRealizadas = 0;
+			sh_mem->edgeList[i].nOperacoesManutencao = 0;
 		}
 		else{
 			return -2;
@@ -180,6 +208,7 @@ int readFile(char* file){
 	
 	
 	sh_mem->highPerformanceFlag = 0;
+	//printf("POST1\n");
 	sem_post(mutex);
 	fclose(f);
 	
@@ -197,8 +226,11 @@ void doTask(int n_instrucoes, int capacidade){
 void *cpu1(void *t){
 	printf("CPU1 is READY\n");
 	int counter = *((int *)t);
+	
 	sem_wait(mutex);
+	//printf("WAIT12\n");
 	close(sh_mem->edgeList[counter].channel[1]);
+	//printf("POST2\n");
 	sem_post(mutex);
 	fd_set read_set;
 	
@@ -207,10 +239,11 @@ void *cpu1(void *t){
 		
 		
 		sem_wait(mutex);
-	
+		//printf("WAIT3\n");
+		sh_mem->edgeList[counter].cpuState1 = AVAILABLE;
 		if(sh_mem->edgeList[counter].state == RUNNING){
 			
-			sh_mem->edgeList[counter].cpuState1 = AVAILABLE;
+			
 			pthread_mutex_lock(&mutexUnnamedPipe);
 		
 			FD_ZERO(&read_set);
@@ -229,7 +262,14 @@ void *cpu1(void *t){
 				
 				int instru = ts.n_instru;
 				int cap = sh_mem->edgeList[counter].capacidade1;
+				
+				int timeAtual = (int)time(NULL);
+				sh_mem->tempoMedioDeEspera += timeAtual - ts.tChegada;
+				sh_mem->tarefasMedioDeEspera++;
+				
+				
 				pthread_mutex_unlock(&mutexUnnamedPipe);
+				//printf("POST3\n");
 				sem_post(mutex);
 				
 				
@@ -237,17 +277,25 @@ void *cpu1(void *t){
 			
 				printf("TAREFA CONCLUIDA\n");
 				char var[MAX_CHAR*2];
+				
+				sem_wait(mutex);
+				//printf("WAIT4\n");
 				sprintf(var, "%s: TASK %d COMPLETED", sh_mem->edgeList[counter].name, ts.id);
 				logFile(var);
+				sh_mem->totalTarefasRealizadas++;
+				sh_mem->edgeList[counter].nTarefasRealizadas++;
+				//printf("POST4\n");
 				sem_post(mutex);
 			}
 			else{
+				//printf("POST5\n");
 				sem_post(mutex);
 			}
 			pthread_mutex_unlock(&mutexUnnamedPipe);
 			
 		}
 		else{
+			//printf("POST6\n");
 			sem_post(mutex);
 		}
 	}
@@ -256,51 +304,85 @@ void *cpu1(void *t){
 }
 
 void *cpu2(void *t){
-	
 	printf("CPU2 is READY\n");
 	int counter = *((int *)t);
-	sem_wait(mutex);	
-	//close(sh_mem->edgeList[counter].channel[1]);//talvez nao necessario? pq ja foi feito no cpu1
+	
+	sem_wait(mutex);
+	//printf("WAIT5\n");
+	close(sh_mem->edgeList[counter].channel[1]);
+	//printf("POST7\n");
 	sem_post(mutex);
 	fd_set read_set;
 	
+	
 	while(1){
+		
+		
 		sem_wait(mutex);
+		//printf("WAIT6\n");
+		sh_mem->edgeList[counter].cpuState2 = AVAILABLE;
 		if(sh_mem->edgeList[counter].state == RUNNING && sh_mem->highPerformanceFlag == 1){
-			sh_mem->edgeList[counter].cpuState2 = AVAILABLE;
+			
+			
+			
 			pthread_mutex_lock(&mutexUnnamedPipe);
+		
 			FD_ZERO(&read_set);
+			
 			FD_SET(sh_mem->edgeList[counter].channel[0], &read_set);
+			
 			if(select(sh_mem->edgeList[counter].channel[0]+1, &read_set, NULL, NULL, &timeout) > 0){
 				taskSimplificada ts;
 				read(sh_mem->edgeList[counter].channel[0], &ts, sizeof(taskSimplificada));
 				printf("%s CPU2 RECEBEU TASK\n", sh_mem->edgeList[counter].name);
-				sh_mem->edgeList[counter].cpuState1 = BUSY;
+				sh_mem->edgeList[counter].cpuState2 = BUSY;
 				printf("AFTER READ:\n");
 				printf("ID: %d, INST: %d\n", ts.id, ts.n_instru);
 				
-				sh_mem->edgeList[counter].timeNextTarefa1 = (int)(ts.n_instru/sh_mem->edgeList[counter].capacidade1);
+				sh_mem->edgeList[counter].timeNextTarefa2 = (int)(ts.n_instru/sh_mem->edgeList[counter].capacidade2);
 				
-				doTask(ts.n_instru, sh_mem->edgeList[counter].capacidade1);
+				int instru = ts.n_instru;
+				int cap = sh_mem->edgeList[counter].capacidade2;
+				
+				int timeAtual = (int)time(NULL);
+				sh_mem->tempoMedioDeEspera += timeAtual - ts.tChegada;
+				sh_mem->tarefasMedioDeEspera++;
+				pthread_mutex_unlock(&mutexUnnamedPipe);
+				//printf("POST8\n");
+				sem_post(mutex);
+				
+				
+				doTask(instru, cap);
 			
 				printf("TAREFA CONCLUIDA\n");
 				char var[MAX_CHAR*2];
+				
+				sem_wait(mutex);
+				//printf("WAIT7\n");
 				sprintf(var, "%s: TASK %d COMPLETED", sh_mem->edgeList[counter].name, ts.id);
 				logFile(var);
+				sh_mem->totalTarefasRealizadas++;
+				sh_mem->edgeList[counter].nTarefasRealizadas++;
+				//printf("POST9\n");
 				sem_post(mutex);
 			}
 			else{
+				//printf("POST10\n");
 				sem_post(mutex);
 			}
 			pthread_mutex_unlock(&mutexUnnamedPipe);
+			
 		}
 		else{
+			//printf("POST11\n");
 			sem_post(mutex);
 		}
 	}
 	
 	pthread_exit(NULL);
 }
+
+
 void changeServerName(int counter, char *str){
 	strcat(sh_mem->edgeList[counter].name, str);
 	//sh_mem->highPerformanceFlag++;//DEBUG
@@ -317,6 +399,7 @@ void edgeServer(int counter){
 	pthread_create(&threads[1], NULL, cpu2, &counter);
 	
 	sem_wait(mutex);
+	//printf("WAIT8\n");
 	printf("%s STARTED\n", sh_mem->edgeList[counter].name);
 	strcpy(var, sh_mem->edgeList[counter].name);
 	logFile(strcat(var, " READY"));
@@ -328,33 +411,61 @@ void edgeServer(int counter){
 	//printf("ALIVE SENT\n");
 	
 	sh_mem->edgeList[counter].state = RUNNING;
-	
+	//printf("POST12\n");
 	sem_post(mutex);
 	
 	while(1){
 		
 		msg.mtype = counter;
+		//printf("debug4\n");
 		msgrcv(msgID, &msg, sizeof(message), counter+1, 0);//aviso que tem que entrar em modo STOPPED
 		//printf("DEBUG4.1 MSG: %s, COUNTER: %d\n", msg.mesg_text, counter+1);
+		
+		/*sem_wait(mutex);
+		printf("WAIT9\n");
+		sh_mem->edgeList[counter].state = STOPPED;
+		
+		while(sh_mem->edgeList[counter].cpuState1 == BUSY && sh_mem->edgeList[counter].cpuState2 == BUSY);*/
+		
+		//printf("debug3\n");
 		sem_wait(mutex);
 		
 		sh_mem->edgeList[counter].state = STOPPED;
-		
-		while(sh_mem->edgeList[counter].cpuState1 == BUSY && sh_mem->edgeList[counter].cpuState2 == BUSY);
-		
+		sh_mem->edgeList[counter].nOperacoesManutencao++;
+		sem_post(mutex);
+		//printf("debug1\n");
+		while(1){
+			sem_wait(mutex);
+			if(sh_mem->edgeList[counter].cpuState1 != BUSY && sh_mem->edgeList[counter].cpuState2 != BUSY){			
+				//sem_post(mutex);
+				break;
+			}
+			
+			sem_post(mutex);
+			
+		}
+		//printf("debug2\n");
 		msg.mtype = QUEUE_MSG_TYPE;
 		strcpy(msg.mesg_text, "ready");
 		
 		msgsnd(msgID, &msg, sizeof(message), 0);
 		
 		printf("%s ENTROU EM MODO STOPPED\n", sh_mem->edgeList[counter].name);
+		strcpy(var, sh_mem->edgeList[counter].name);
+		logFile(strcat(var, " HAS STOPPED"));
+		//printf("POST13\n");
 		sem_post(mutex);
 		//printf("WAITING...\n");
 		msgrcv(msgID, &msg, sizeof(message), counter+1, 0);//aviso que pode voltar ao modo RUNNING
 		//printf("GETTING BACK TO RUNNING\n");
+		
 		sem_wait(mutex);
+		//printf("WAIT10\n");
 		sh_mem->edgeList[counter].state = RUNNING;
 		printf("%s VOLTOU AO MODO RUNNING\n", sh_mem->edgeList[counter].name);
+		strcpy(var, sh_mem->edgeList[counter].name);
+		logFile(strcat(var, " IS BACK TO RUNNING"));
+		//printf("POST14\n");
 		sem_post(mutex);
 	}
 	
@@ -378,8 +489,12 @@ void removeTarefasImpo__Prio(){
 				t->data.prio = 1000;
 				printf("Task Removed: tMax->%d, timeAtual->%d\n", t->data.tMax, timeAtual);
 				logFile("TASK REMOVED");
+				
 				sem_wait(mutex);
+				//printf("WAIT11\n");
 				sh_mem->currentTasksN--;
+				sh_mem->totalTarefasNaoRealizadas++;
+				//printf("POST15\n");
 				sem_post(mutex);
 			}
 			else{
@@ -468,9 +583,11 @@ int verificaExistirTarefas(){
 
 int tryDispatchTask(tasks* t){
 	printf("TRY DISPATCH\n");
-	sem_wait(mutex); 
 	
+	sem_wait(mutex); 
+	//printf("WAIT12\n");
 	for(int i=0; i < sh_mem->nEdgeServers; i++){
+		if(sh_mem->edgeList[i].state == RUNNING){
 		if(sh_mem->edgeList[i].cpuState1 == AVAILABLE){
 			//printf("HERE\n");
 			//printf("D. %d\n", (int)(t->data.n_instrucoes/sh_mem->edgeList[i].capacidade1) + (int)time(NULL));
@@ -483,10 +600,12 @@ int tryDispatchTask(tasks* t){
 				taskSimplificada ts;
 				ts.id = t->data.id;
 				ts.n_instru = t->data.n_instrucoes;
+				ts.tChegada = t->data.tChegada;
 				write(sh_mem->edgeList[i].channel[1], &ts, sizeof(taskSimplificada));
 				//removetask
 				t->data.id = -1;
 				sh_mem->currentTasksN--;
+				//printf("POST16\n");
 				sem_post(mutex);
 				return 1;
 			}
@@ -498,56 +617,70 @@ int tryDispatchTask(tasks* t){
 					taskSimplificada ts;
 					ts.id = t->data.id;
 					ts.n_instru = t->data.n_instrucoes;
+					ts.tChegada = t->data.tChegada;
 					write(sh_mem->edgeList[i].channel[1], &ts, sizeof(taskSimplificada));
 					//removetask
 					t->data.id = -1;
 					sh_mem->currentTasksN--;
+					//printf("POST17\n");
 					sem_post(mutex);
 					return 1;
 				}
 			}
 		}
+		}
 	}
 	printf("NO DISPATCH\n");
+	//printf("POST18\n");
 	sem_post(mutex);
 	return -1;
 }
 
 int taskValida(tasks* t){
 	//printf("DEBUG4\n");
+	
 	sem_wait(mutex); 
+	//printf("WAIT13\n");
 	//printf("DEBUG5\n");
 	for(int i=0; i < sh_mem->nEdgeServers; i++){
 		if((int)(t->data.n_instrucoes/sh_mem->edgeList[i].capacidade1) + (int)time(NULL) < t->data.tMax){
+			//printf("POST19\n");
 			sem_post(mutex);
 			return 1;
 		}
 		if((int)(t->data.n_instrucoes/sh_mem->edgeList[i].capacidade2) + (int)time(NULL) < t->data.tMax){
+			//printf("POST20\n");
 			sem_post(mutex);
 			return 1;
 		}
 	}
 	t->data.id=-1;//removed
 	sh_mem->currentTasksN--;
+	sh_mem->totalTarefasNaoRealizadas++;
+	//printf("POST21\n");
 	sem_post(mutex);
 	return -1;
 }
 
 int verificaCPU(){
 	//printf("DEBUG1\n");
+	
 	sem_wait(mutex);
+	//printf("WAIT14\n");
 	//printf("DEBUG2\n");
 	for(int i=0; i < sh_mem->nEdgeServers; i++){
 		if(sh_mem->edgeList[i].state == RUNNING){
 			//printf("ESTADO1: %d, ESTADO2: %d\n", sh_mem->edgeList[i].cpuState1, sh_mem->edgeList[i].cpuState2);
 			if(sh_mem->edgeList[i].cpuState1 == AVAILABLE){
 				//printf("ENCONTROU CPU1\n");
+				//printf("POST22\n");
 				sem_post(mutex);
 				return 1;
 			}
 			if(sh_mem->highPerformanceFlag == 1){
 				if(sh_mem->edgeList[i].cpuState2 == AVAILABLE){
 					//printf("ENCONTROU CPU2\n");
+					//printf("POST23\n");
 					sem_post(mutex);
 					return 2;
 				}
@@ -555,6 +688,7 @@ int verificaCPU(){
 		}
 	}
 	//printf("FLAG: %d\n", sh_mem->highPerformanceFlag);
+	//printf("POST24\n");
 	sem_post(mutex);
 	//printf("NAO ENCONTROU CPU DISPONIVEL\n");
 	
@@ -562,10 +696,13 @@ int verificaCPU(){
 }
 
 void printServers(){
+	
 	sem_wait(mutex);
+	//printf("WAIT15\n");
 	for(int i=0; i < sh_mem->nEdgeServers; i++){
 		printf("%s, STATE: %d\n", sh_mem->edgeList[i].name, sh_mem->edgeList[i].state);
 	}
+	//printf("POST25\n");
 	sem_post(mutex);
 }
 
@@ -599,7 +736,7 @@ void *dispatcher(void *t){
 			t=t->next;
 		}
 		pthread_mutex_unlock(&mutexFila);
-		sleep(2);
+		
 	}
 }
 
@@ -611,7 +748,8 @@ void taskManager(){
 	
 	timeout.tv_sec = 0;
 	timeout.tv_usec = 10;
-	sem_wait(mutex); printf("DEBUG6\n");
+	
+	sem_wait(mutex); //printf("WAIT16\n");
 	
 	sem_unlink("SERVERMUTEX");
 	serverMutex = sem_open("SERVERMUTEX", O_CREAT | O_EXCL, 0700, 1);
@@ -627,7 +765,7 @@ void taskManager(){
 			exit(0);
 		}
 	}
-	
+	//printf("POST26\n");
 	sem_post(mutex); 
 	
 	sem_wait(serverMutex);//para apenas avancar quando os servers todos tiverem preparados
@@ -706,8 +844,11 @@ void insertFila(int id, int n_inst, int tMax){
 			t->data.tChegada = (int)time(NULL);
 			//printf("TIME: %d\n", t->tMax);
 			t->data.prio = 0;
+			
 			sem_wait(mutex);
+			//printf("WAIT17\n");
 			sh_mem->currentTasksN++;
+			//printf("POST27\n");
 			sem_post(mutex);
 			logFile("NEW TASK INSERTED");
 			printf("NEW TASK INSERTED\n");
@@ -717,6 +858,9 @@ void insertFila(int id, int n_inst, int tMax){
 	}
 	logFile("QUEUE FULL => TASK REFUSED");
 	printf("Fila ocupada, tarefa eliminada\n");
+	sem_wait(mutex);
+	sh_mem->totalTarefasNaoRealizadas++;
+	sem_wait(mutex);
 }
 
 int tempoEspera(){
@@ -735,46 +879,65 @@ void monitor(){
 	logFile("PROCESS MONITOR CREATED");
 	while(1){
 		while(1){
+			
 			sem_wait(mutex);
-			if(sh_mem->currentTasksN / sh_mem->slots > 0.8 && tempoEspera() == 1){
+			//printf("WAIT18\n");
+			char var[100];
+			float a = sh_mem->currentTasksN / sh_mem->slots;
+			sprintf(var, "%f // %d", a, tempoEspera());
+			logFile2(var);
+			if(a > 0.8 && tempoEspera() == 1){
 				sh_mem->highPerformanceFlag = 1; //ativa highperformance mode
 				printf("HIGHPERFORMANCE FLAG ATIVATED\n");
+				logFile("HIGHPERFORMANCE FLAG ATIVATED");
+				//printf("POST28\n");
 				sem_post(mutex);
 				break;
 			}
+			//printf("POST29\n");
 			sem_post(mutex);
 		}
 		while(1){
+			
 			sem_wait(mutex);
-			if(sh_mem->currentTasksN / sh_mem->slots <= 0.2){
+			//printf("WAIT19\n");
+			float a = sh_mem->currentTasksN / sh_mem->slots;
+			if(a <= 0.2){
 				sh_mem->highPerformanceFlag = 0; //desliga highperformance mode
 				printf("HIGHPERFORMANCE FLAG DEATIVATED\n");
+				logFile("HIGHPERFORMANCE FLAG DEATIVATED");
+				//printf("POST30\n");
 				sem_post(mutex);
 				break;
 			}
+			//printf("POST31\n");
 			sem_post(mutex);
 		}
 	}
 	exit(0);
 }
 
+
 void maintenanceManager(){
 	logFile("PROCESS MAINTENANCE MANAGER CREATED");
+	
 	sem_wait(mutex);
+	//printf("WAIT20\n");
 	int totalServers = sh_mem-> nEdgeServers;
+	//printf("POST32\n");
 	sem_post(mutex);
 	message msg;
 	srandom(getpid());
-	/*int randomStop;
+	int randomStop;
 	int randomIntervalo;
 	int min = 1;
-	int max = 5;*/
+	int max = 5;
 	//printf("DEBUG5\n");
 	for(int i = 0; i < totalServers; i++){//esperar pela mensagem de alive dos servers
 		msgrcv(msgID, &msg, sizeof(message), QUEUE_MSG_TYPE, 0);
 		printf("MESSAGE RECEIVED\n");
 	}
-	/*
+	
 	//printf("STARTING 1234567777234234234\n");
 	while(1){
 	for(int i = 0; i < totalServers; i++){
@@ -795,7 +958,7 @@ void maintenanceManager(){
 		//printf("SLEEP->%dsec\n", randomIntervalo);
 		sleep(randomIntervalo);
 	}
-	}*/
+	}
 }
 
 void print_taskList(){
@@ -810,7 +973,9 @@ void print_taskList(){
 }
 
 void print_SHM(){
+	
 	sem_wait(mutex);
+	//printf("WAIT21\n");
 	printf("SHM:\n");
 	printf("Slots: %d\n", sh_mem->slots);
 	printf("MaxWait: %d\n", sh_mem->maxWait);
@@ -818,6 +983,7 @@ void print_SHM(){
 	for(int i=0; i < sh_mem->nEdgeServers; i++){
 		printf("%s: %d, %d\n", sh_mem->edgeList[i].name, sh_mem->edgeList[i].capacidade1, sh_mem->edgeList[i].capacidade2);
 	}
+	//printf("POST33\n");
 	sem_post(mutex);
 }
 
@@ -845,6 +1011,7 @@ void sigCleanup(){
 
 void cleanup(){//falta dar free a memoria da fila de tasks
 	printf("CLEANUP\n");
+	
 	msgctl(msgID, IPC_RMID, 0);
 	
 	shmctl(shmid, IPC_RMID, 0);
@@ -866,6 +1033,26 @@ void stats(){
 	if((int)systemManagerPID == (int)getpid()){
 		logFile("SIGNAL SIGTSTP RECEIVED");
 		printf("Stats: TODO\n");
+		
+		sem_wait(mutex);
+		
+		printf("TOTAL TAREFAS: %d\n", sh_mem->totalTarefasRealizadas);
+		
+		if(sh_mem->tarefasMedioDeEspera == 0)
+			printf("TEMPO1 MEDIO DE ESPERA: 0 sec\n");
+		else	
+			printf("TEMPO2 MEDIO DE ESPERA: %d sec\n", sh_mem->tempoMedioDeEspera / sh_mem->tarefasMedioDeEspera);
+		
+		
+		
+		for(int i=0; i < sh_mem->nEdgeServers; i++){
+			printf("%s -> %d tarefas, %d operacoes de manutencao\n", sh_mem->edgeList[i].name, sh_mem->edgeList[i].nTarefasRealizadas, sh_mem->edgeList[i].nOperacoesManutencao);
+		}
+		
+		printf("TOTAL DE TAREFAS NAO EXECUTADAS: %d\n", sh_mem->totalTarefasNaoRealizadas);
+		sleep(5);
+		sem_post(mutex);
+		
 	}
 }
 
@@ -876,6 +1063,9 @@ void systemManager(char *filename){
 	signal(SIGTSTP, stats);
 	
 	FILE *f = fopen("log.txt", "w");
+	fclose(f);
+	
+	f = fopen("debug.txt", "w");
 	fclose(f);
 	
 	logFile("OFFLOAD SIMULATOR STARTING");
@@ -900,7 +1090,14 @@ void systemManager(char *filename){
 	mutex = sem_open("MUTEX", O_CREAT | O_EXCL, 0700, 1);
 	
 	sem_wait(mutex);
+	//printf("WAIT23\n");
 	sh_mem->currentTasksN = 0;
+	sh_mem->totalTarefasRealizadas = 0;
+	sh_mem->tempoMedioDeEspera = 0;
+	sh_mem->tarefasMedioDeEspera = 0;
+	sh_mem->totalTarefasNaoRealizadas = 0;
+	
+	//printf("POST35\n");
 	sem_post(mutex);
 	//aqui a variavel systemManagerPID fica a 0 por alguma razao
 	systemManagerPID = getpid();
